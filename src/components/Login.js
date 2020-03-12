1b1a663e31849937eb4fb96930ee68606082fbe9
manzi-guev <manziguevara@gmail.com>
2020-03-12 15:36:15 +0200

    ch(frontend): setting up react router [Finishes #170766025]

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Barefoot Nomad</h1>
+      </div>
+    );
+  }
+}
+export default Login;
